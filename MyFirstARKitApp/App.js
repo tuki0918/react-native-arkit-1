f9bce4be70bd593c6fca1e451ba798930d682235
Yuta Yamamoto <tuki0918@gmail.com>
2018-05-16 22:34:16 +0900

    ios arkit code

--- MyFirstARKitApp/App.js
+++ MyFirstARKitApp/App.js
@@ -1,58 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+import { AppRegistry, View } from 'react-native';
+import { ARKit } from 'react-native-arkit';
+export default class App extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+    <ARKit
+        style={{ flex: 1 }}
+        debug // debug mode will show feature points detected and 3D axis
+        planeDetection // turn on plane detection
+        lightEstimation // turn on light estimation
+        >
+        <ARKit.Box
+        pos={{ x: 0, y: 0, z: 0 }}
+        shape={{ width: 0.1, height: 0.1, length: 0.1, chamfer: 0.01 }}
+        />
+        </ARKit>
+        </View>
     );
-  }
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('MyFirstARKitApp', () => App);
